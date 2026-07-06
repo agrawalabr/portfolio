60fac8cd11d43faa5acb19e6bccab7289581bc85
“agrawalabr” <agrawalabr@gmail.com>
2026-07-06 19:58:52 -0400

    v2

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -4,7 +4,6 @@ import parse from 'html-react-parser';
 import details from '../../assets/data.json'
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
-import LeftPanel from '../Left-panel/left-panel.js';
 
 const Profile = () => {
     
